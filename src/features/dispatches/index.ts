718fceab9579d419ff67bac6932ef0ca16097b35
Atharva Singh <atharvasingh.24@gmail.com>
2026-04-22 20:28:29 +0530

    feat(dispatches): re-export Dispatch type + helpers from barrel

--- src/features/dispatches/index.ts
+++ src/features/dispatches/index.ts
@@ -1,2 +1,4 @@
 export { DispatchesContent } from './DispatchesContent';
 export { DispatchCard } from './DispatchCard';
+export type { Dispatch, DispatchPlatform } from './types';
+export { hasAnyPublishedUrl, publishedPlatforms, primaryDispatchUrl } from './types';
